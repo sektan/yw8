a65353e62266b4633279ad7c4167f8d9a2a7fe5b
LAPTOP-4N6KCO21\taniasekhon <tania.sekhon@dishq.in>
2016-11-15 14:39:06 +0530

    google analytics

---
+++ app/src/main/java/com/dishq/buzz/Analytics/GoogleAnalyticsApplication.java
@@ -0,0 +1,11 @@
+package com.dishq.buzz.Analytics;
+
+import android.app.Application;
+
+/**
+ * Created by dishq on 15-11-2016.
+ */
+
+public class GoogleAnalyticsApplication extends Application {
+    
+}
